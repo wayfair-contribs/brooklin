e83fdec22754e81b42b38058767718f42a7a4665
arli <arli@wayfair.com>
2020-12-02 19:13:11 -0500

    Add new translators to serialize key as single value instead of a json record to mirror Kafka Connect behavior

---
+++ datastream-common/src/main/java/com/linkedin/datastream/common/translator/TranslatorConstants.java
@@ -0,0 +1,17 @@
+package com.linkedin.datastream.common.translator;
+
+/**
+ * Various well known config keys used in translators
+ */
+public class TranslatorConstants {
+
+    /**
+     * Represents the key in the Avro record
+     */
+    protected static final String AVRO_SCHEMA_KEY = "key";
+
+    /**
+     *  Represents the record name in the Avro schema
+     */
+    protected static final String AVRO_SCHEMA_RECORD_NAME = "sqlRecord";
+}

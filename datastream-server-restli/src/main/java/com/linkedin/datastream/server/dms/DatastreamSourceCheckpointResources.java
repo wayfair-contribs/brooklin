8f11e51abd27e0404bf02b86610aa4ae1f73b132
arli <arli@wayfair.com>
2021-01-20 11:00:15 -0500

    New endpoint to allow checkpoints to be viewed and updated

--- datastream-server-restli/src/main/java/com/linkedin/datastream/server/dms/DatastreamSourceCheckpointResources.java
+++ datastream-server-restli/src/main/java/com/linkedin/datastream/server/dms/DatastreamSourceCheckpointResources.java
@@ -114,7 +114,6 @@ public class DatastreamSourceCheckpointResources extends SimpleResourceTemplate<
 
     private ConnectorHealth buildSourceCheckpoint(Datastream datastream) {
         ConnectorHealth sourceCheckpointDetail = new ConnectorHealth();
-//        sourceCheckpointDetail.setDatastreamName(datastream.getName());
         sourceCheckpointDetail.setTasks(buildTaskSourceCheckpoint(datastream));
 
         return sourceCheckpointDetail;

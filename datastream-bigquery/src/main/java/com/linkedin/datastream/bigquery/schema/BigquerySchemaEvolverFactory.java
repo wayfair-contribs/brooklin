0c2dde2ab77ec9635a71d84379d8e0ae0b4d2352
Chris Fontana <cfontana@wayfair.com>
2021-01-25 16:55:41 -0500

    Minor refactoring and code cleanup

--- datastream-bigquery/src/main/java/com/linkedin/datastream/bigquery/schema/BigquerySchemaEvolverFactory.java
+++ datastream-bigquery/src/main/java/com/linkedin/datastream/bigquery/schema/BigquerySchemaEvolverFactory.java
@@ -5,29 +5,13 @@
  */
 package com.linkedin.datastream.bigquery.schema;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import com.linkedin.datastream.common.VerifiableProperties;
-
 /**
  * A factory to construct BigquerySchemaEvolver instances.
  */
 public class BigquerySchemaEvolverFactory {
 
-    private static final BigquerySchemaEvolver SIMPLE_SCHEMA_EVOLVER = new DynamicBigquerySchemaEvolver();
-    private static final BigquerySchemaEvolver NOOP_SCHEMA_EVOLVER = new FixedBigquerySchemaEvolver();
-
-    /**
-     * A factory method that constructs a BigquerySchemaEvolver instance from properties.
-     * @param properties the VerifiableProperties
-     * @return an instance of BigquerySchemaEvolver
-     */
-    public static BigquerySchemaEvolver createBigquerySchemaEvolver(final VerifiableProperties properties) {
-        final String schemaEvolverTypeStr = properties.getString("type", BigquerySchemaEvolverType.fixed.name());
-        return createBigquerySchemaEvolver(BigquerySchemaEvolverType.valueOf(schemaEvolverTypeStr));
-    }
+    private static final BigquerySchemaEvolver DYNAMIC_SCHEMA_EVOLVER = new DynamicBigquerySchemaEvolver();
+    private static final BigquerySchemaEvolver FIXED_SCHEMA_EVOLVER = new FixedBigquerySchemaEvolver();
 
     /**
      * Factory method to create a BigquerySchemaEvolver based on the BigquerySchemaEvolverType
@@ -38,10 +22,10 @@ public class BigquerySchemaEvolverFactory {
         final BigquerySchemaEvolver schemaEvolver;
         switch (schemaEvolverType) {
             case dynamic:
-                schemaEvolver = SIMPLE_SCHEMA_EVOLVER;
+                schemaEvolver = DYNAMIC_SCHEMA_EVOLVER;
                 break;
             case fixed:
-                schemaEvolver = NOOP_SCHEMA_EVOLVER;
+                schemaEvolver = FIXED_SCHEMA_EVOLVER;
                 break;
             default:
                 throw new IllegalStateException("Unsupported BigquerySchemaEvolverType: " + schemaEvolverType);
@@ -49,16 +33,4 @@ public class BigquerySchemaEvolverFactory {
         return schemaEvolver;
     }
 
-    /**
-     * A factory method that constructs a map of schema evovler names to BigquerySchemaEvolver instance from properties.
-     * @param properties the VerifiableProperties
-     * @return a map of schema evolver name to BigquerySchemaEvolver
-     */
-    public static Map<String, BigquerySchemaEvolver> createBigquerySchemaEvolvers(final VerifiableProperties properties) {
-        final List<String> names = properties.getStringList("names");
-        return names.stream().collect(Collectors.toMap(name -> name, name -> {
-            final VerifiableProperties schemaEvolverProperties = new VerifiableProperties(properties.getDomainProperties(name));
-            return createBigquerySchemaEvolver(schemaEvolverProperties);
-        }));
-    }
 }
